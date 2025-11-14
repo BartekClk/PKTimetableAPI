4e4280162333a61bdddd537956d679bc53968a9d
Bartek <clkbartek@gmail.com>
2025-11-14 10:39:27 +0100

    Changes to be committed: 	modified:   main.py

--- main.py
+++ main.py
@@ -311,3 +311,7 @@ def getWeektype(type:str = None):
         return [{"weekType":week.get()}]
     else:
         return [{"weekType":week.get(earlyChange=True)}]
+    
+@app.get("/hours")
+def getHours():
+    return [{"id":1,"start":"07:30","end":"08:15"},{"id":2,"start":"08:15","end":"09:00"},{"id":3,"start":"09:15","end":"10:00"},{"id":4,"start":"10:00","end":"10:45"},{"id":5,"start":"11:00","end":"11:45"},{"id":6,"start":"11:45","end":"12:30"},{"id":7,"start":"12:45","end":"13:30"},{"id":8,"start":"13:30","end":"14:15"},{"id":9,"start":"14:30","end":"15:15"},{"id":10,"start":"15:15","end":"16:00"},{"id":11,"start":"16:15","end":"17:00"},{"id":12,"start":"17:00","end":"17:45"},{"id":13,"start":"18:00","end":"18:45"},{"id":14,"start":"18:45","end":"19:30"},{"id":15,"start":"19:45","end":"20:30"},{"id":16,"start":"20:30","end":"21:15"}]
